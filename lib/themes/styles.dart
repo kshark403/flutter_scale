46c36069f88517ab77c99e08f7651113bbe9a79a
unknown <shaovapan@hotmaile.com>
2023-08-26 21:34:00 +0700

    Update styles

--- lib/themes/styles.dart
+++ lib/themes/styles.dart
@@ -7,17 +7,16 @@ class AppTheme {
   // Custom text theme
   static final customTextTheme = const TextTheme(
     bodyMedium: TextStyle(
-      fontFamily: 'IMBlexSansThai',
       fontSize: 16,
     ),
     bodyLarge: TextStyle(
-      fontFamily: 'IMBlexSansThai',
       fontSize: 24,
     ),
   );
 
   // light theme
   static final ThemeData lightTheme = ThemeData(
+    fontFamily: 'IBMPlexSansThai',
     textTheme: customTextTheme,
     primaryColor: primary,
     primaryColorDark: primaryDark,
@@ -41,6 +40,7 @@ class AppTheme {
 
   // Dark theme
   static final ThemeData darkTheme = ThemeData(
+    fontFamily: 'IBMPlexSansThai',
     textTheme: customTextTheme,
     primaryColor: primary,
     primaryColorDark: primaryDark,
