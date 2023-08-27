185b78058a1f8ce760c4ed9403908f28dc4ab584
unknown <shaovapan@hotmaile.com>
2023-08-27 09:02:50 +0700

    Update login form

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -10,14 +10,105 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // สร้าง key สำหรับ form
+  final formKey = GlobalKey<FormState>();
+
+  // สร้างตัวแปรไว้เก็บค่า username และ password
+  late String _username, _password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('เข้าสู่ระบบ'),
-      ),
-      body: Center(
-        child: Text('หน้าเข้าสู่ระบบ'),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  SizedBox(
+                    height: 180,
+                  ),
+                  Image.asset(
+                    'assets/images/mylogo.png',
+                    height: 150,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    // width: 250,
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'กรุณากรอกชื่อผู้ใช้งาน';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _username = value!.toString().trim();
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    // width: 250,
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'กรุณากรอกรหัสผ่าน';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _password = value!.toString().trim();
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // ตรวจสอบค่าจาก form
+                        if (formKey.currentState!.validate()) {
+                          formKey.currentState!.save();
+
+                          print('Username: $_username');
+                          print('Password: $_password');
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5))),
+                      child: Text(
+                        'เข้าสู่ระบบ',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
